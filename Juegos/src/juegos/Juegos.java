190a169b0cfbeb25d3b0aa0b1e0cf9dbcb3230ec
FelipeSalcedo-max <u1803256@unimilitar.edu.co>
2020-03-11 20:07:29 -0500

    /

--- Juegos/src/juegos/Juegos.java
+++ Juegos/src/juegos/Juegos.java
@@ -5,17 +5,35 @@
  */
 package juegos;
 
-/**
- *
- * @author RetailAdmin
- */
+import java.util.Scanner;
+import javax.swing.JOptionPane;
 public class Juegos {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner objetoIn= new Scanner(System.in);
+    int nElementos=Integer.parseInt(JOptionPane.showInputDialog("Cuantos datos quiere ordenar?"));
+    int vector[]=new int[nElementos];
+    int aux;
+    for(int i=0;i<nElementos;i++){    
+    System.out.println("Ingrese el dato "+(i+1));
+    vector[i]=objetoIn.nextInt();
+    }
+    // algoritmo burbuja:
+        for (int  i = 0;  i < (nElementos-1);  i++) {
+            for (int  j = 0;  j <(nElementos-1);  j++) {
+                if(vector[j]>vector[j+1]){
+                 aux=vector[j];
+                 vector[j]=vector[j+1];
+                 vector[j+1]=aux;
+                }
+            }
+        }
+        for (int  i = 0;  i <nElementos;  i++) {
+            System.out.print(" "+vector[i]);
+        } 
     }
     
 }
